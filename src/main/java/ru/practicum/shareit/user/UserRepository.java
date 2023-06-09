6e4518dd9373f1811e1fd2a27c997cf40605ab9c
Safromanov <Safroman-ov@yandex.ru>
2023-06-09 15:01:50 +0700

    fix

--- src/main/java/ru/practicum/shareit/user/UserRepository.java
+++ src/main/java/ru/practicum/shareit/user/UserRepository.java
@@ -1,8 +1,9 @@
 package ru.practicum.shareit.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long>, QuerydslPredicateExecutor<User> {
 
     long countByEmail(String email);
 }
